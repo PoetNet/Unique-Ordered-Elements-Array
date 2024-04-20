15bf5cdf7c67f56ab3f34b56a2fdbadf3684e181
Marfutin Roman <roman.marfutin.external@atom.team>
2024-04-20 12:34:07 +0300

    Create UniqueElementsGetter and two examples

---
+++ UniqueOrderedElementsArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using UniqueOrderedElementsArray;
+
+var result1 = UniqueElementsGetter.GetUniqueElementsInOrder("XBBHhLM");
+var result2 = UniqueElementsGetter.GetUniqueElementsInOrder([1, 1, 5, 5, 7, 8, 8, 8]);
+
+var resultString1 = string.Join(' ', result1);
+var resultString2 = string.Join(' ', result2);
+
+Console.WriteLine(resultString1);
+Console.WriteLine(resultString2);
+
+Console.ReadLine();
---
+++ UniqueOrderedElementsArray/UniqueElementsGetter.cs
@@ -0,0 +1,18 @@
+﻿namespace UniqueOrderedElementsArray;
+public class UniqueElementsGetter
+{
+    public static Queue<T> GetUniqueElementsInOrder<T>(IEnumerable<T> items)
+    {
+        Queue<T> arranged = new ();
+        T firstItem = items.First();
+
+        foreach (T currentItem in items)
+        {
+            if (currentItem.Equals(firstItem)) arranged.Enqueue(currentItem);
+            else if (currentItem.Equals(arranged.Last())) { }
+            else arranged.Enqueue(currentItem);
+        }
+
+        return arranged;
+    }
+}
